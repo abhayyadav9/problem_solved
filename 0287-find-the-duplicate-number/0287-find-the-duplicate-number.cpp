f30a1b36520b2d537f8f946c98723e5867d4c4e8
Abhay Yadav <yadavabhay8227@gmail.com>
2026-02-08 08:51:47 +0530

    Time: 0 ms (100%), Space: 65 MB (89.44%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,16 +1,44 @@
+// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+
+//         int n = nums.size();
+
+//         for(int i =1; i<n ; i++){
+//             if(nums[i -1] == nums[i] ){
+//                 return nums[i];
+//             }
+//         }
+//         return 0;
+        
+//     }
+// };
+
+
+//optimize way using the floyds algo
+
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int slow= nums[0];
+        int fast = nums[0];
 
-        int n = nums.size();
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow !=fast);
 
-        for(int i =1; i<n ; i++){
-            if(nums[i -1] == nums[i] ){
-                return nums[i];
-            }
+        slow = nums[0];
+
+        while(slow != fast){
+            slow= nums[slow];
+            fast = nums[fast];
         }
-        return nums[0];
+
+
+        return slow;
         
     }
 };
